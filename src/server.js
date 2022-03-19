7e6d5d94a3a2042c4ece7e26c200ec02c37457c8
AmanRaj <amanraj7166@gmail.com>
2022-03-19 11:58:49 +0530

    TEST

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const server = http.createServer(app);
 
 //server.listen(PORT, console.log(`Server is running on port ${PORT}`));
 
-app.listen(process.env.PORT || 5000, function(){
+// app.listen(process.env.PORT || 5000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//   });
+
+  app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
